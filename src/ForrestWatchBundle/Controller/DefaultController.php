9017c3b680f6f78b75aef426f2f936714c395ee5
r0bot <r0bot@student.edu>
2016-11-28 19:17:09 +0100

    formularz zrobiony w TWIGu, wzorowany na bootstrappie

--- src/ForrestWatchBundle/Controller/DefaultController.php
+++ src/ForrestWatchBundle/Controller/DefaultController.php
@@ -15,8 +15,16 @@ class DefaultController extends Controller
         $repository = $this->getDoctrine()->getRepository('ForrestWatchBundle:Questions');
         $questions = $repository->findAll();
         
+        $repository = $this->getDoctrine()->getRepository('ForrestWatchBundle:Region');
+        $regions = $repository->findAll();
+        
+        $repository = $this->getDoctrine()->getRepository('ForrestWatchBundle:Kingdom');
+        $kingdom = $repository->findAll();
+        
         return $this->render('ForrestWatchBundle:Default:index.html.twig', array(
             'questions' => $questions,
+            'regions' => $regions,
+            'kingdom' => $kingdom
         ));
         
     }
